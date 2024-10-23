cf26e1657bf03b6458c58aad17fb0f472dd9bac8
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-23 16:20:17 +0200

    Solved problem to repeat string "hey" 3 times
    Made simple loop to repeat "hey" and conctact
    
    It 3 times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
 const repeatString = function() {
+    let str = "hey";
+    let num = 3;
+    let repStr = "";
 
+    for(let i = 0; i < num; i++){
+        repStr += str;
+    }
+    return repStr;
 };
 
 // Do not edit below this line
