09450d8f7f473e3a9a87203790b7cd7d1ae2a822
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-24 15:43:03 +0200

    Passed all the test make a for loop to sum += i
    Made if statements to pass all the trival cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
+    
+    let start = 0;
+    let end = 0;
+    let sum = 0;
 
+    if((numA || numB) < 0){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(numA) || !Number.isInteger(numB)){
+        return "ERROR";
+    }
+    else if(numA > numB){
+        start = numB;
+        end = numA;
+    }
+    else {
+        start = numA;
+        end = numB;
+    }
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
