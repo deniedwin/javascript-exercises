ae26f66a8cc070a652f7a652e2b09ddf4119c8d5
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-24 12:10:44 +0200

    Solved the test using for loop and array methods
    By changing the string to array first
    
    Then reverse array and storing into new array
    
    Then changing rev array to a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let strToArr = str.split("");
+    let revStrToArr = [];
+    let index = 0;
 
+    for(let i = strToArr.length-1; i >= 0; i--){
+        revStrToArr[index] = strToArr[i];
+        index++;
+    }
+    return revStrToArr.join("");
 };
 
 // Do not edit below this line
