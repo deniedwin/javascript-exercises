85692659051e4514177e44a2bbcd2ed649a441c4
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-24 14:24:49 +0200

    Comment first solution did not use rest parameter
    First I did not use the rest paramenter
    
    This caused problem, if the rest paramenter
    
    Is used then all tests are passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < items.length; j++){
+            if(arr[i] === items[j]){
+                arr.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
