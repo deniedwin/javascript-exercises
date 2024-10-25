98918d07f489eaf863f63cbb439790a5b0c96442
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 10:12:58 +0200

    Made two functions rounded with Math rounded
    Two simple wikipedia functions
    
    And rounded with math round method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let degreeInCel = (degree-32)*(5/9);
+  let degreeInCelRound = Math.round(degreeInCel * 10)/ 10;
+  return degreeInCelRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let degreeInFar = (degree*(9/5)+32);
+  let degreeInFarRound = Math.round(degreeInFar * 10)/ 10;
+  return degreeInFarRound;
 };
 
 // Do not edit below this line
