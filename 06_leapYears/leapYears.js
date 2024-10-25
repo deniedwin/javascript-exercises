03d36b6129a8569e5a4e05c2cd646055e14ef331
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 09:40:09 +0200

    Made first logic function for leap year
    Used flow chart drawing to visuallize this

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if((num%400 === 0) || num%4 ===0){
+        if(num%100 === 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
