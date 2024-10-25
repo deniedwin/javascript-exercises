57ab305599ec41988ea4d131a25c319f5067ad3c
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 09:54:43 +0200

    Function does not pass all the tests
    Working on different logic to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,15 @@
 const leapYears = function(num) {
-    if((num%400 === 0) || num%4 ===0){
+    if(num%4 === 0){
         if(num%100 === 0){
             return false;
         }
         else{
-            return true;
+            if(num%400 === 0){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
     }
     else {
