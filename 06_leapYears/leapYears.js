bbe39a92a08d66fde609ef6f522033d405fda9db
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-10-25 10:03:01 +0200

    Made new function using AND OR logic
    This passed all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,8 @@
 const leapYears = function(num) {
-    if(num%4 === 0){
-        if(num%100 === 0){
-            return false;
-        }
-        else{
-            if(num%400 === 0){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+    if(num%4===0 && (num%100!==0 || num%400===0)){
+        return true;
     }
-    else {
+    else{
         return false;
     }
 };
