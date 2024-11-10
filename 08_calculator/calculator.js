1a832b1ab459e171fa3f1c226c4f84d2684bdcf3
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-10 12:45:21 +0100

    Passed all the function test of calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = arr.reduce((acu, item) => acu + item, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let mult = arr.reduce((acu, item) => acu * item, 1);
+  return mult
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let pwr = Math.pow(a, b);
+  return pwr;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num <= 1){
+    return 1;
+  }
+  else{
+    let fact = 1;
+    for(let i = num; i >= 1; i--){
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
