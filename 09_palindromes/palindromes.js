7fe405be7d7abf0d26533393792fc3d7100fec8b
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-10 13:17:27 +0100

    Passed first palindrome test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    for(let i = 0; i < myString.length; i++){
+        if(myString[i] !== myString[myString.length-(i+1)]){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 };
 
 // Do not edit below this line
