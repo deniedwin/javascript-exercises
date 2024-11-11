7f134f9247efed1c3e7dc5b4fb03db2e978fa1a6
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-11 16:45:50 +0100

    Made function to calculate fibonacci sequence
    By using for loop and doing push to array
    
    Perhaps there is a more optimal way to do this
    
    Without the need to save each fibonacci number
    
    To the end of the array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    //change num to number in case it is a string
+    let strToNum = Number(num);
+    if(strToNum === 0){
+        return 0;
+    }
+    else if(strToNum <= 0){
+        return "OOPS";
+    }
+    let fibo = [1,1,2,3];
+    for(let i = 4; i <= strToNum; i++){
+        let nextFibo = 0;
+        nextFibo = fibo[i-1] + fibo[i-2];
+        fibo[i] = nextFibo;
+        fibo.push(nextFibo);
+    }
+    return fibo[strToNum-1];
 };
 
 // Do not edit below this line
