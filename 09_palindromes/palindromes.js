3bfa9762ddf068e7eec418504364ff9a960cc471
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-11 15:36:48 +0100

    Made function to check if string is palindrome
    Split string to array to check chars seperatly
    
    Once checked join back again to make string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,18 @@
 const palindromes = function (myString) {
-    for(let i = 0; i < myString.length; i++){
-        if(myString[i] !== myString[myString.length-(i+1)]){
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
+    let cleanString = ""; //string to remove punctuation and other chars
+    let reverseString = ""; //clean string but reverses order
+    const alpha = "abcdefghijklmnopqrstuvwxyz0123456789"; //string that includes desirec chars
+    //myString is first converted to lowercase
+    //then it split into an array
+    //then retunrs only the items that are in alpha (others are discardted)
+    //then join back to string
+    cleanString = myString.toLowerCase().split("").filter((item) => (alpha.includes(item))).join("");
+    //reverseString is split to array
+    //then reversed
+    //then join back to string
+    reverseString = cleanString.split("").reverse().join("");
+    //returns true or false if the clean string is the same as reverse string
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
