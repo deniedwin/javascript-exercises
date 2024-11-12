eb92c5a474b42be3eb4972cc6e0d4f809c97ab77
deniedwin <24278798+deniedwin@users.noreply.github.com>
2024-11-12 11:16:12 +0100

    Solve function using forEach method of the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const d = new Date();
+    let old = 0;
+    let oldIndex = 0;
+    arr.forEach((item, index) => {
+        if(item.yearOfBirth === undefined){
+            item.yearOfBirth = d.getFullYear();
+        }
+        else if(item.yearOfDeath === undefined){
+            item.yearOfDeath = d.getFullYear();
+        }
+        
+        let age = item.yearOfDeath - item.yearOfBirth;
+        if(age > old){
+            old = age;
+            oldIndex = index;
+        }
+    });
+    return arr[oldIndex];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
